8224869d2d3ae54754aea9c711db799b0609903d
egi <egijaelani1010@gmail.com>
2025-12-07 23:56:39 +0700

    Refactor deleteAlbum.php for improved readability

--- tugas9/deleteAlbum.php
+++ tugas9/deleteAlbum.php
@@ -4,30 +4,26 @@ $album = new Album();
 
 $id = $_GET['id'];
 
-// 1. Ambil data album sebelum dihapus untuk mendapatkan nama file cover
 $data = $album->getAlbumById($id); 
 
-// 2. Lakukan penghapusan dari database
 if ($album->deleteAlbum($id)) {
     
-    // 3. Jika penghapusan dari DB sukses, hapus file cover (jika ada)
     if (
         !empty($data['cover']) && 
-        $data['cover'] != 'default_album.jpg' && // Hindari hapus file default
+        $data['cover'] != 'default_album.jpg' &&
         file_exists("images/" . $data['cover'])
     ) {
-        unlink("images/" . $data['cover']); // Fungsi untuk menghapus file fisik
+        unlink("images/" . $data['cover']);
     }
     
-    // 4. Beri notifikasi dan redirect
     echo "<script>
             alert('Data berhasil dihapus'); 
             window.location.href='dataAlbum.php';
           </script>";
 } else {
-    // 5. Tangani kegagalan
     echo "<script>
             alert('Gagal menghapus data'); 
             window.location.href='dataAlbum.php';
           </script>";
+
 }
