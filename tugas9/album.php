3dd2d5d8e4f840ad040e2072b36087593506b30a
egi <egijaelani1010@gmail.com>
2025-12-07 23:59:17 +0700

    Fix formatting issues in album.php

--- tugas9/album.php
+++ tugas9/album.php
@@ -18,18 +18,16 @@ class Album {
     }
 
     public function getAlbumById($id) {
-    // PENTING: Menggunakan id_album untuk SELECT
     $result = $this->db->select("tb_album", ["id_album" => $id]); 
     return $result[0];
     }
 
     public function updateAlbum($id, $data) {
-        // PENTING: Menggunakan id_album untuk UPDATE
         return $this->db->update("tb_album", $data, ["id_album" => $id]);
     }
 
     public function deleteAlbum($id) {
-        // PENTING: Menggunakan id_album untuk DELETE
         return $this->db->delete("tb_album", ["id_album" => $id]);
     }
 }
+
